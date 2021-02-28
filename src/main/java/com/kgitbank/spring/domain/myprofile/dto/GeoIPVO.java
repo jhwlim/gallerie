34b54e3e2e35ee190891a84a47ae66099fe5adc3
82105 <82105@DESKTOP-7R346KH>
2021-02-28 17:32:07 +0900

    google지도 업데이트(90)

--- src/main/java/com/kgitbank/spring/domain/myprofile/dto/GeoIPVO.java
+++ src/main/java/com/kgitbank/spring/domain/myprofile/dto/GeoIPVO.java
@@ -1,15 +1,14 @@
-package com.kgitbank.spring.domain.geoIPTest.controller;
+package com.kgitbank.spring.domain.myprofile.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class GeoIP {
+public class GeoIPVO {
 	private String ipAddress;
     private String city;
     private String latitude;
     private String longitude;
-    
-  
+
 }
